430ae6b30386bb28826264c078e9c43afb4c9ac4
MauricioAB <dev.mauricioalezandre@gmail.com>
2021-12-08 10:44:00 -0300

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FirstComponent } from './components/first/first.component';
+import { SecondComponent } from './components/second/second.component';
+
+const routes: Routes = [
+  {
+    path: 'first-component',
+    component: FirstComponent
+  },
+  {
+    path: 'second-component',
+    component: SecondComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
