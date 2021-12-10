5ea4ad4569a0dc52158ddfd1de579b98f0619f3e
MauricioAB <dev.mauricioalezandre@gmail.com>
2021-12-10 08:01:53 -0300

    Routes tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ChildAComponent } from './components/child-a/child-a.component';
+import { ChildBComponent } from './components/child-b/child-b.component';
+
 import { FirstComponent } from './components/first/first.component';
 import { SecondComponent } from './components/second/second.component';
 
 const routes: Routes = [
   {
     path: 'first-component',
-    component: FirstComponent
+    component: FirstComponent, // this is the component with the <router-outlet> in the template
+    children: [
+      {
+        path: 'child-a', // child route path
+        component: ChildAComponent, // child route component that the router renders
+      },
+      {
+        path: 'child-b',
+        component: ChildBComponent, // another child route component that the router renders
+      },
+    ],
   },
   {
     path: 'second-component',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './components/first/first.component';
 import { SecondComponent } from './components/second/second.component';
+import { ChildAComponent } from './components/child-a/child-a.component';
+import { ChildBComponent } from './components/child-b/child-b.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
-    SecondComponent
+    SecondComponent,
+    ChildAComponent,
+    ChildBComponent
   ],
   imports: [
     BrowserModule,
